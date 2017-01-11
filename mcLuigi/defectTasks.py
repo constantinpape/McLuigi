2fa2928c57121c6278e6fd534452c726e0ae357d
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2017-01-11 14:17:53 +0100

    Implemented defect detection

--- mcLuigi/defectTasks.py
+++ mcLuigi/defectTasks.py
@@ -26,7 +26,7 @@ workflow_logger = logging.getLogger(__name__)
 config_logger(workflow_logger)
 
 
-class OversegmentationStatistics(luigi.Task):
+class OversegmentationPatchStatistics(luigi.Task):
 
     pathToSeg = luigi.Parameter()
     patchSize = luigi.Parameter()
@@ -57,27 +57,112 @@ class OversegmentationStatistics(luigi.Task):
             return n_segs_z
 
         # sequential for debugging
-        segs_per_patch = []
-        for z in xrange(seg.shape[0]):
-            segs_per_patch.extend(extract_patch_statistics_slice(z))
+        #segs_per_patch = []
+        #for z in xrange(seg.shape[0]):
+        #    segs_per_patch.extend(extract_patch_statistics_slice(z))
 
         # parallel
-        #with futures.ThreadPoolExecutor(max_workers=PipelineParameter().nThreads) as executor:
-        #    tasks = []
-        #    for z in xrange(seg.shape[0]):
-        #        tasks.append(executor.submit(extract_patch_statistics_slice, z))
-        #    segs_per_patch = []
-        #    for fut in tasks:
-        #        segs_per_patch.extend(fut.result())
+        with futures.ThreadPoolExecutor(max_workers=PipelineParameter().nThreads) as executor:
+            tasks = []
+            for z in xrange(seg.shape[0]):
+                tasks.append(executor.submit(extract_patch_statistics_slice, z))
+            segs_per_patch = []
+            for fut in tasks:
+                segs_per_patch.extend(fut.result())
 
+        # TODO calculate histogram to have a closer look at the stats
         mean = np.mean(segs_per_patch)
         std  = np.std(segs_per_patch)
-        stats = np.array([mean,std])
-        print stats
-        self.output().write(stats, 'data')
+        histo, _ = np.histogram(segs_per_patch, bins = 32)
 
+        stats = np.zeros([histo.shape[0]+2])
+        stats[0] = mean
+        stats[1] = std
+        stats[2:] = histo
+
+        self.output().write(stats, 'data')
 
     def output(self):
         segFile = os.path.split(self.pathToSeg)[1][:-3]
-        save_path = os.path.join( PipelineParameter().cache, "OversegmentationStatistics_%s.h5" % (segFile,) )
+        save_path = os.path.join( PipelineParameter().cache, "OversegmentationPatchStatistics_%s.h5" % (segFile,) )
         return HDF5DataTarget(save_path)
+
+
+class DefectPatchDetection(luigi.Task):
+
+    pathToSeg = luigi.Parameter()
+    patchSize = luigi.Parameter()
+    patchOverlap = luigi.Parameter()
+    # patches that have below (defectThreshold*mean) number of segment get marked as defected
+    # this needs some tuning, 15 % sounds like a reasonable initial guess
+    defectThreshold = luigi.Parameter(default = .15)
+
+    def requires(self):
+        return {"seg" : ExternalSegmentation(self.pathToSeg),
+                "stats" : OversegmentationPatchStatistics(self.pathToSeg,self.patchSize)}
+
+    def run(self):
+        inp = self.input()
+        seg = inp["seg"]
+        stats = inp["stats"].read()
+
+        mean_num_segs = stats[0]
+
+        seg.open()
+        out = self.output()
+        # TODO compression for nifty hdf5
+        out.open(seg.shape)
+
+        ny = long(seg.shape[1])
+        nx = long(seg.shape[2])
+
+        patch_shape = [self.patchSize,self.patchSize]
+        patch_overlap = [long(self.patchOverlap),long(self.patchOverlap)]
+
+        workflow_logger.info("Running defect detection:")
+
+        def detect_patches_z(z):
+            seg_z = seg.read([long(z),0L,0L],[z+1,ny,nx])
+            patches = nifty.tools.blocking( roiBegin = [0L,0L], roiEnd = [ny,nx], blockShape = patch_shape )
+            # get number of segments for patches in this slice
+            n_segs_z = []
+            n_defected = 0
+            for patch_id in xrange(patches.numberOfBlocks):
+                patch = patches.getBlockWithHalo(patch_id, patch_overlap).outerBlock
+                patch_begin, patch_end = patch.begin, patch.end
+                patch_slicing = np.s_[patch_begin[0]:patch_end[0],patch_begin[1]:patch_end[1]]
+                # get number of segments in this segment
+                n_segments = np.unique(seg_z[patch_slicing]).shape[0]
+                # if this number is below threshold, mark the patch as defected
+                # TODO need to properly tune this, because false positive hurt really bad...
+                # maybe have different numbers for different thresholds to factor in some uncertainties
+                if n_segments < self.defectThreshold * mean_num_segs:
+                    this_patch = np.ones( tuple(map(lambda x,y:x-y, patch_end, patch_end)), dtype=np.uint8)
+                    this_begin = [long(z)] + patch_begin
+                    out.write(this_begin, this_patch)
+                    n_defected += 1
+            return n_defected
+
+        # non parallel for debugging
+        #defects_per_slice = []
+        #for z in xrange(seg.shape[0]):
+        #    defects_per_slice.append(detect_patches_z(z))
+
+        with futures.ThreadPoolExecutor(max_workers = PipelineParameter().nThreads) as executor:
+            tasks = []
+            for z in xrange(seg.shape[0]):
+                tasks.append(executor.submit(detect_patches_z,z))
+            defects_per_slice = [fut.result() for fut in tasks]
+
+        # log the defects
+        workflow_logger.info("Total number of defected patches: %i" % np.sum(defects_per_slice))
+        for z in xrange(seg.shape[0]):
+            if defects_per_slice[z] > 0:
+                workflow_logger.info("Slice %i has %i defected patches." % (z,defects_per_slice[z]))
+
+        out.close()
+
+    def output(self):
+        segFile = os.path.split(self.pathToSeg)[1][:-3]
+        save_path = os.path.join( PipelineParameter().cache, "DefectPatchDetection_%s.h5" % (segFile,) )
+        return HDF5VolumeTarget(save_path, dtype = 'uint8')
