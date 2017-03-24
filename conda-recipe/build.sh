52f300a892ecc755a40a4bc9e06bdc06d360607b
Constantin Pape <c.pape@gmx.net>
2017-03-24 18:01:12 -0400

    Copying missing source files

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -4,7 +4,10 @@ mkdir -p ${PREFIX}/mc_luigi
 # copy all the python sources
 cp mc_luigi/*Tasks.py ${PREFIX}/mc_luigi
 cp mc_luigi/__init__.py ${PREFIX}/mc_luigi
+cp mc_luigi/taskSelection.py ${PREFIX}/mc_luigi
+cp mc_luigi/pipelineParameter.py ${PREFIX}/mc_luigi
+cp mc_luigi/customTargets.py ${PREFIX}/mc_luigi
 cp -r mc_luigi/tools ${PREFIX}/mc_luigi
 
-echo "${PREFIX}/mc_luigi" > ${PREFIX}/lib/python2.7/site-packages/mc_luigi.pth
+echo "${PREFIX}" > ${PREFIX}/lib/python2.7/site-packages/mc_luigi.pth
 python -m compileall ${PREFIX}/mc_luigi
