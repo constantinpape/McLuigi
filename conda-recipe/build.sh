222bd0260afea4fa338b0be70c59212f31ec6cff
Constantin Pape <c.pape@gmx.net>
2017-03-24 14:13:55 -0400

    Only including relevant files in conda pkg

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,6 +1,10 @@
 # cf. https://github.com/ilastik/nature_methods_multicut_pipeline/blob/master/build/multicut_pipeline/build.sh
-# copy all the python sources
 mkdir -p ${PREFIX}/mcLuigi
-cp -r mcLuigi/* ${PREFIX}/mcLuigi
+
+# copy all the python sources
+cp  mcLuigi/*Tasks.py ${PREFIX}/mcLuigi
+cp mcLuigi/__init__.py ${PREFIX}/mcLuigi
+cp -r mcLuigi/tools ${PREFIX}/mcLuigi
+
 echo "${PREFIX}/mcLuigi" > ${PREFIX}/lib/python2.7/site-packages/mcLuigi.pth
 python -m compileall ${PREFIX}/mcLuigi
