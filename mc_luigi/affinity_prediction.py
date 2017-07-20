ae3a6c9d593ae80668808a892cd7e37c7c72b0e4
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2017-07-20 11:21:51 -0400

    Adding scripts for affinity prediction with gunpowder

---
+++ mc_luigi/affinity_prediction.py
@@ -0,0 +1,107 @@
+import os
+import argparse
+
+# TODO can we import local python files in the docker env?
+# if yes, do:
+# from pipelineParameter import PipelineParameter
+
+from gunpowder import *
+from gunpowder.caffe import *
+
+def predict(
+    raw_path,
+    out_folder,
+    out_name,
+    net_architecture,
+    net_weights
+):
+    assert os.path.exists(raw_path), raw_path
+    assert os.path.exists(net_folder), net_folder
+    assert os.path.exists(out_folder), out_folder
+    assert os.path.exists(net_architecture), net_architecture
+    assert os.path.exists(net_weights), net_weights
+
+    # TODO this should not be hard coded here, should fetch it from somewhere instead
+    input_size = Coordinate((84, 268, 268))
+    output_size = Coordinate((56, 56, 56))
+
+    # the size of the receptive field of the network
+    context = (input_size - output_size) / 2
+
+    # a chunk request that matches the dimensions of the network, will be used
+    # to chunk the whole volume into batches of this size
+    chunk_request = BatchRequest()
+    chunk_request.add_volume_request(VolumeTypes.RAW, input_size)
+    chunk_request.add_volume_request(VolumeTypes.PRED_AFFINITIES, output_size)
+
+    #source = Hdf5Source(raw_path, datasets={VolumeTypes.RAW: 'volumes/raw'})
+    source = Hdf5Source(raw_path, datasets={VolumeTypes.RAW: 'data'})
+
+    # save the prediction
+    save = Snapshot(
+        every=1,
+        output_dir=out_folder,
+        output_filename=out_name
+    )
+
+    # build the pipeline
+    pipeline = (
+
+            # raw sources
+            source +
+
+            # normalize raw data
+            Normalize() +
+
+            # pad raw data
+            Pad({ VolumeTypes.RAW: (100, 100, 100) }) +
+
+            # shift to [-1, 1]
+            IntensityScaleShift(2, -1) +
+
+            ZeroOutConstSections() +
+
+            # do the actual prediction
+            Predict(prototxt, weights, use_gpu=0) +
+
+            PrintProfilingStats() +
+
+            Chunk(chunk_request) +
+
+            # save
+            save
+    )
+
+    with build(pipeline) as p:
+
+        # get the ROI of the whole RAW region from the source
+        raw_roi = source.get_spec().volumes[VolumeTypes.RAW]
+
+        # request affinity predictions for the whole RAW ROI
+        whole_request = BatchRequest({
+                VolumeTypes.RAW: raw_roi,
+                VolumeTypes.PRED_AFFINITIES: raw_roi.grow(-context, -context)
+            })
+
+        print("Requesting " + str(whole_request) + " in chunks of " + str(chunk_request))
+
+        p.request_batch(whole_request)
+
+
+def parse_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('raw_path', type=str)
+    parser.add_argument('out_path', type=str)
+    parser.add_argument('net_architecture', type=str)
+    parser.add_argument('net_weights', type=str)
+    args = parser.parse_args()
+
+    out_path = parser.out_path
+    out_folder, out_name = os.path.split(out_path)
+    return args.raw_path, out_folder, out_name, args.net_architecture, args.net_weights
+
+
+if __name__ == "__main__":
+    # TODO if we have pipeline parameter, things would be nicer, because we don't have to pass
+    # as many parameters as arguments
+    predict(*parse_input())
---
+++ mc_luigi/affinity_prediction.sh
@@ -0,0 +1,20 @@
+# TODO understand this properly and adapt to use own gunpowder version
+# TODO pass correct arguments to 'affinity_predition.py'
+
+export NAME=$(basename "$PWD")
+
+nvidia-docker rm -f $NAME
+
+NV_GPU=0 nvidia-docker run --rm \
+    -u `id -u $USER` \
+    -v $(pwd):/workspace \
+    -v /groups/saalfeld/home/papec:/groups/saalfeld/home/papec \
+    -w /workspace \
+    --name $NAME \
+    funkey/gunpowder:v0.2 \
+    python -u predict.py
+
+
+# if want to use own gunpowder:
+# export pythonpath with own gunpowder
+# before 
